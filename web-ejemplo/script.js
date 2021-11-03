593db3b39417c583fa1b64c7e21f5967b50bbc31
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-11-03 10:19:48 +0100

    first commit

---
+++ web-ejemplo/script.js
@@ -0,0 +1,86 @@
+/*
+let firstName = 'Miguel'
+const lastName = 4
+var isDeveloper = true
+
+console.log(firstName)
+firstName = 'Pepito'
+console.log(firstName)
+*/
+
+/*
+const firstName = 'Miguel'
+const firstNameWithUppercase = firstName.toUpperCase()
+
+console.log(firstNameWithUppercase)
+*/
+
+/*
+const list = []
+list.push(1)
+
+console.log(list)
+*/
+
+/*
+const list = []
+list.push(157)
+
+console.log(list[0])
+*/
+
+/*
+const list = []
+const anotherList = list.concat(157)
+
+console.log(list[0])
+console.log(anotherList)
+*/
+
+/*
+const list = []
+const anotherList = list.concat(157)
+
+const persona = {
+	name: 'Miguel',
+	twitter: '@midudev',
+	age: 18,
+	isDeveloper: true,
+	links: ['https://midu.tube', 'https://midu.live'],
+}
+
+console.log(persona.name)
+console.log(persona.links[0])
+const field = 'twitter'
+console.log(persona[field])
+
+const sumar = (operando1, operando2) => {
+	console.log(operando1)
+	console.log(operando2)
+	return operando1 + operando2
+}
+
+// const resultado = sumar(2, 2)
+
+console.log(sumar(2, 2))
+*/
+
+/*
+console.log(restar(2 - 2))
+
+const sumar = (a, b) => {
+	console.log(a)
+	console.log(b)
+	return a + b
+}
+
+function restar(a, b) {
+	return a - b
+}
+
+const op1 = 8
+const op2 = 2
+
+console.log(sumar(op1, op2))
+console.log(sumar(2, 'dos'))
+*/
